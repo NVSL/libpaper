3ea45fec5c323dd10122ac61de6c8bac1472e9bf
Jian Yang <jianyang@ucsd.edu>
2018-11-18 01:57:29 +0000

    move to NVSL

--- autobuild/buildpaper.sh
+++ autobuild/buildpaper.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
+PAPER_ROOT="$HOME/papers"
 PAPER=$1
+GS_BUCKET="gs://nvsl-paper"
 SAVED_PWD=`pwd`
 
-# TODO add .lock
-cd ../$PAPER
-git pull
+buildonce()
+{
+    git pull
+    make clean
+    make
+    gsutil cp *.pdf $GS_BUCKET/$PAPER/
+    gsutil cp paper.pdf $GS_BUCKET/$PAPER.pdf
+}
+
+cd $PAPER_ROOT
+
+if [ ! -d $PAPER ]; then
+	echo "Cloning repo $PAPER."
+	git clone git@github.com:NVSL/$PAPER.git
+	(cd $PAPER && make)
+fi
+
+cd $PAPER_ROOT/$PAPER
 echo cmdline: $* >> $SAVED_PWD/build.log
-echo sed: $SED >> $SAVED_PWD/build.log
-make clean
-make
-gsutil cp paper.pdf gs://jian-storage/$1.pdf
-#git reset --hard
+
+# Tag rebuild when the paper is being built
+if [ -f .build ]; then
+	touch .rebuild
+	return 0
+fi
+
+touch .build
+buildonce
+
+while [ -f .rebuild ]; do
+	rm -f .rebuild
+	buildonce
+done
+
+rm .build -f
